9081999a65b1ce36a3bac13ce1b4a8e4cb973aa7
Conrad <corta.dev@gmail.com>
2022-06-29 05:10:36 -0400

    Got the tabs workin' ooooweeee k bb

---
+++ src/components/Global.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Content, Heading } from "react-bulma-components";
+
+const Global = () => {
+  return (
+    <Container>
+      <Heading>Global</Heading>
+      <Content>
+        <p id="main-intro">
+          The Global tab will allow the user to view all of the regions in their
+          business at once. All of the data will be compiled and shown here.
+          There will be decision made as to what is best to show here. Judging
+          by what is most impactful on a global perspective, some of the
+          criteria of what to show may be different that what we choose to
+          display in the regional tabs. Seeing as how we want the regional tabs
+          to be more granular, there will probably be different data scripts
+          needed for each.
+        </p>
+      </Content>
+    </Container>
+  );
+};
+export default Global;
--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,55 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Heading, Hero, Tabs } from "react-bulma-components";
 import Introduction from "./Introduction";
+import Global from "./Global";
 
 const TopBanner = () => {
+  // create state variable for activeTab
+  const [activeTab, setActiveTab] = useState("intro");
+
+  // create a function to set the state to a string passed as an argument
+
+  // create a function called activeTabContent to find the index in the tabList array where
+  //    the content we want rendered is.
+  //    Return the component stored there with the Array.findIndex method
+  const activeTabContent = () => {
+    let activeIndex = tabList.findIndex((tab) => tab.name === activeTab);
+    return tabList[activeIndex].component;
+  };
+
   return (
     <Box color={"grey"}>
       <Heading textSize={3} mb={2}>
         Athnos Dashboard Utility
       </Heading>
-      {/* <Tabs type="boxed" mb={2}> */}
-      {/* <Tabs.Tab id={"intro"} onClick={handleClick}>
-          Intro
-          </Tabs.Tab>
-          <Tabs.Tab id={"global"} onClick={handleClick}>
-          Global
-        </Tabs.Tab> */}
-      {/* </Tabs>
+      <Tabs
+        tabList={tabList}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        type="boxed"
+        mb={2}
+      >
+        {/* inside this component, send a prop tabList (an array of objects key val pairs),
+          a prop actTab (a string with the name of the tab we want active), and a prop to set
+          the state of the banner.
+
+          The end result should be mapping over the array here to create individual tabs
+            with those props passed in. Something like:
+             <Tab tabList={tabList}
+                  activeTab={activeTab}
+                  setActiveTab=setActiveTab(activeTab)
+        */}
+        {tabList.map((tab) => (
+          <Tab
+            title={tab.title}
+            name={tab.name}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        ))}
+      </Tabs>
+      {/* add a key which will be the current active tab,
+          pass this key to <TopBanner />. Add a prop of content,
+          this prop will be the return of function that finds the component based
+          on an index of tabsList (an array of key val pairs).
+        */}
       <Hero color={"secondary"} size={"small"}>
         <Hero.Body py={1}>
-          <Introduction />
-        </Hero.Body> */}
-      {/* </Hero> */}
+          {/* in here, we will render the result of the function activeTabContent */}
+          {/* <Introduction /> */}
+          {activeTabContent()}
+        </Hero.Body>
+      </Hero>
     </Box>
   );
 };
 
 const tabList = [
-  { component: Introduction, name: "Intro" },
-  { component: "Global Tab LOL", name: "Global" },
+  { component: <Introduction />, name: "intro", title: "Intro" },
+  { component: <Global />, name: "global", title: "Global" },
 ];
 
-const Tab = ({ name, active }) => {
-  return <Tabs.Tab active={active}>{name}</Tabs.Tab>;
-};
-const TabGroup = () => {
+const Tab = ({ title, name, activeTab, setActiveTab }) => {
   return (
-    // <>
-    // {tabList.map(tab=>{<Tab})}
-    // </>
-    // <>
-    <Tabs type="boxed" mb={2}>
-      {tabList.map((tab) => {
-        <Tab name={tab.name} />;
-      })}
-    </Tabs>
-    //   <Hero color={"secondary"} size={"small"}>
-    //     <Hero.Body py={1}>
-    //       <Introduction />
-    //     </Hero.Body>
-    //   </Hero>
-    // </>
+    <Tabs.Tab active={activeTab === name} onClick={() => setActiveTab(name)}>
+      {title}
+    </Tabs.Tab>
   );
 };
+
 export default TopBanner;
